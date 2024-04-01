81fc4ad7794f777d60560c6ebd0704dc2cda7d0b
dino.shin <troas96@naver.com>
2024-04-01 16:40:50 +0900

    feat: 구입 개수(PurchaseCount) 구현
    Co-authored-by: zeroberry <98147817+zeroberry@users.noreply.github.com>

---
+++ src/test/java/lotto/PurchaseCountTest.java
@@ -0,0 +1,26 @@
+package lotto;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@SuppressWarnings("NonAsciiCharacters")
+class PurchaseCountTest {
+
+    @ParameterizedTest(name = "개수가 {0}일 때")
+    @ValueSource(ints = {1, 2, 5, 10, 100, 1000})
+    void 올바르게_구입_개수를_생성할_수_있다(final int count) {
+        Assertions.assertDoesNotThrow(() -> new PurchaseCount(count));
+    }
+
+    @ParameterizedTest(name = "개수가 {0}일 때")
+    @ValueSource(ints = {0, -1, -20, -100, -1000})
+    void 구입_개수가_자연수가_아니면_예외_처리한다(final int count) {
+        assertThatThrownBy(() -> new PurchaseCount(count))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("구입 개수는 1개 이상이어야 합니다.");
+    }
+}
+
