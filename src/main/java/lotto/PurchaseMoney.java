56626d42b0e198bc84d44d4bd3eed359157b8846
dino.shin <troas96@naver.com>
2024-04-01 16:33:29 +0900

    feat: 구입 금액(PurchaseMoney) 구현
    Co-authored-by: zeroberry <98147817+zeroberry@users.noreply.github.com>

---
+++ src/main/java/lotto/PurchaseMoney.java
@@ -0,0 +1,39 @@
+package lotto;
+
+import java.util.Objects;
+
+public class PurchaseMoney {
+
+    private static final int ZERO = 0;
+    private static final int SINGLE_LOTTO_COST = 1000;
+    private static final String INVALID_AMOUNT_MESSAGE = "숫자는 1000단위의 자연수여야 합니다.";
+
+    private final int amount;
+
+    public PurchaseMoney(final int amount) {
+        validateAmount(amount);
+        this.amount = amount;
+    }
+
+    private void validateAmount(final int amount) {
+        if (amount % SINGLE_LOTTO_COST != ZERO) {
+            throw new IllegalArgumentException(INVALID_AMOUNT_MESSAGE);
+        }
+        if (amount <= ZERO) {
+            throw new IllegalArgumentException(INVALID_AMOUNT_MESSAGE);
+        }
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final PurchaseMoney that = (PurchaseMoney) o;
+        return amount == that.amount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount);
+    }
+}
