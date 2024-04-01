5e57ef056a4aa67142262db0fb69675fdbd87d54
dino.shin <troas96@naver.com>
2024-04-01 17:00:59 +0900

    feat: 당첨금(WinningMoney) 구현
    Co-authored-by: zeroberry <98147817+zeroberry@users.noreply.github.com>

---
+++ src/main/java/lotto/WinningMoney.java
@@ -0,0 +1,40 @@
+package lotto;
+
+import java.math.BigInteger;
+import java.util.Objects;
+
+public class WinningMoney {
+
+    private static final int ZERO = 0;
+    public static final String INVALID_AMOUNT_MESSAGE = "당첨금은 음수일 수 없습니다.";
+
+    private final BigInteger amount;
+
+    public WinningMoney(final BigInteger amount) {
+        validateAmount(amount);
+        this.amount = amount;
+    }
+
+    private void validateAmount(final BigInteger amount) {
+        if (isNegativeAmount(amount)) {
+            throw new IllegalArgumentException(INVALID_AMOUNT_MESSAGE);
+        }
+    }
+
+    private boolean isNegativeAmount(final BigInteger amount) {
+        return amount.compareTo(BigInteger.ZERO) < ZERO;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final WinningMoney that = (WinningMoney) o;
+        return Objects.equals(amount, that.amount);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount);
+    }
+}
---
+++ src/test/java/lotto/WinningMoneyTest.java
@@ -0,0 +1,37 @@
+package lotto;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.math.BigInteger;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+@SuppressWarnings("NonAsciiCharacters")
+class WinningMoneyTest {
+
+    @ParameterizedTest(name = "금액이 {0}일 떄")
+    @ValueSource(ints = {0, 50000, 100000, 2000000, Integer.MAX_VALUE})
+    void 올바르게_당첨금을_생성할_수_있다(final int amount) {
+        assertDoesNotThrow(() -> new WinningMoney(BigInteger.valueOf(amount)));
+    }
+
+    @ParameterizedTest(name = "금액이 {0}일 떄")
+    @ValueSource(ints = {-1, -1000, -10000, -50000})
+    void 당첨금이_음수이면_예외_처리한다(final int amount) {
+        // given
+        final BigInteger winningAmount = BigInteger.valueOf(amount);
+
+        // when & then
+        assertThatThrownBy(() -> new WinningMoney(winningAmount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("당첨금은 음수일 수 없습니다.");
+    }
+
+    @ParameterizedTest(name = "금액이 {0}일 떄")
+    @ValueSource(strings = {"123456789123456789", "123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789"})
+    void 당첨금이_int_범위값을_넘어도_올바르게_생성할_수_있다(final String amount) {
+        assertDoesNotThrow(() -> new WinningMoney(new BigInteger(amount)));
+    }
+}
