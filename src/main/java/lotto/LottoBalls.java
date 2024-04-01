17b7411e6c46363be48302bb3decb0976467c41a
dino.shin <troas96@naver.com>
2024-04-01 16:01:09 +0900

    feat: 로또 그룹(LottoBalls) 구현
    Co-authored-by: zeroberry <98147817+zeroberry@users.noreply.github.com>

---
+++ src/main/java/lotto/LottoBalls.java
@@ -0,0 +1,39 @@
+package lotto;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LottoBalls {
+
+    private static final int BALLS_SIZE = 6;
+    private static final String INVALID_BALLS_SIZE_MESSAGE = "로또 볼은 6개가 존재해야 합니다.";
+    private static final String DUPLICATED_BALLS_MESSAGE = "로또 그룹에서 볼은 중복될 수 없습니다.";
+
+    private final List<LottoBall> balls;
+
+    public LottoBalls(final List<Integer> numbers) {
+        validateNumbersSize(numbers);
+        validateDuplicateBalls(numbers);
+
+        this.balls = numbers.stream()
+                .map(LottoBall::new)
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+    private void validateNumbersSize(final List<Integer> numbers) {
+        if (numbers.size() != BALLS_SIZE) {
+            throw new IllegalArgumentException(INVALID_BALLS_SIZE_MESSAGE);
+        }
+    }
+
+    private void validateDuplicateBalls(final List<Integer> numbers) {
+        if (removedDuplicatedSize(numbers) != numbers.size()) {
+            throw new IllegalArgumentException(DUPLICATED_BALLS_MESSAGE);
+        }
+    }
+
+    private int removedDuplicatedSize(final List<Integer> numbers) {
+        return new HashSet<>(numbers).size();
+    }
+}
---
+++ src/test/java/lotto/LottoBallsTest.java
@@ -0,0 +1,39 @@
+package lotto;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+@SuppressWarnings("NonAsciiCharacters")
+class LottoBallsTest {
+
+    @Test
+    void 올바르게_로또_그룹을_생성할_수_있다() {
+        assertDoesNotThrow(() -> new LottoBalls(List.of(1, 2, 3, 4, 5, 6)));
+    }
+
+    @Test
+    void 로또_그룹에_6개_이외의_로또_볼이_존재할_시_예외_처리한다() {
+        // given
+        final List<Integer> numbers = List.of(1, 2);
+
+        // when & then
+        assertThatThrownBy(() -> new LottoBalls(numbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("로또 볼은 6개가 존재해야 합니다.");
+    }
+
+    @Test
+    void 중복된_로또_볼이_존재할_시_예외_처리한다() {
+        // given
+        final List<Integer> numbers = List.of(1, 2, 7, 7, 23, 45);
+
+        // when & then
+        assertThatThrownBy(() -> new LottoBalls(numbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("로또 그룹에서 볼은 중복될 수 없습니다.");
+    }
+}
