8d47b4c57a7b9aaff77307e6479a292dfcc3ddf7
zeroberry <98147817+zeroberry@users.noreply.github.com>
2024-04-03 17:32:25 +0900

    fix: message 오타 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -16,8 +16,8 @@ public class OutputView {
     private static final String SECOND = "SECOND";
     private static final String FIRST = "FIRST";
     private static final String COUNT_MESSAGE = "개";
-    private static final String FIFTH_MESSAGE = "3개 일 (5000)- ";
-    private static final String FOURTH_MESSAGE = "4개 일 (50000)- ";
+    private static final String FIFTH_MESSAGE = "3개 일치 (5000)- ";
+    private static final String FOURTH_MESSAGE = "4개 일치 (50000)- ";
     private static final String THIRD_MESSAGE = "5개 일치 (1500000원)- ";
     private static final String SECOND_MESSAGE = "5개 일치, 보너스 볼 일치(30000000원) - ";
     private static final String TOTAL_REVENUE_MESSAGE = "총 수익률은 ";
